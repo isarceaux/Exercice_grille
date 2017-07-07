e5e786a1d9c907821744123caeadd8ec1ec6a9e0
isarceaux <isarceaux>
2017-07-07 02:30:13 +0200

    Ajout de la fonctionnalité qui donne aux carrés une dimension qui s'ajuste! Le js modifie le cssgit add .

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,13 @@ console.log(number);
 
 //Creating the string sequence corresponding to the number to have the right grid
 var htmlstring = "" 
-//"<tr><td></td><td></td></tr><tr><td></td><td></td></tr>";
 
 for (i = 0; i < number; i++) 
 { 
 	htmlstring += "<tr>";
 		for (j = 0; j < number; j++) 
 		{ 
-		    htmlstring += "</td><td>";
-		    console.log(j);
+		    htmlstring += "<td></td>";
 		}
 	htmlstring += "</tr>";
 }
@@ -21,3 +19,23 @@ console.log(htmlstring);
 
 //pushing the new html into the table
 document.getElementById("table").innerHTML = htmlstring;
+
+//Automatic setup of width and height, the base is a 600px square
+
+////Calculation of the right width and heigth
+var pixel_size = Math.floor(600/number);
+console.log(pixel_size);
+var pixel_name = ''+pixel_size+'px';
+console.log(pixel_name);
+
+//Implementation of the width of each square
+var els = document.getElementById("table").getElementsByTagName("td");
+for(var i=0;i<number;i++){
+  els[i].style.width += pixel_name;    
+}
+
+//Implementation of the height of each square
+var els = document.getElementById("table").getElementsByTagName("tr");
+for(var i=0;i<number;i++){
+  els[i].style.height = pixel_name;    
+}
